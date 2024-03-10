79aebf02ce6ca7529caa771feb16fe225aff8fdf
Olaf Schlüter <tallinn1960@yahoo.de>
2024-03-10 19:20:11 +0100

    use a deterministic pseudo-random number generator

--- benches/contest.rs
+++ benches/contest.rs
@@ -2,26 +2,13 @@
 
 use contest::{count_submatrices, count_submatrices_fastest, count_submatrices_ref};
 use criterion::{criterion_group, criterion_main, Criterion};
-use rand::distributions::Distribution;
+use test_utils::generate_test_grid;
 
 fn count_submatrices_benchmark(c: &mut Criterion) {
-    // create an 1000 * 1000 grid with random positive i32 numbers in the range
-    // 0..1000
+    let (grid, k) = generate_test_grid();
 
-    let mut grid = vec![];
-    let k = 500000000; // let's use a big number to make the benchmark more interesting
-    let rng = rand::thread_rng();
-    let mut generator = rand::distributions::Uniform::new(0, 1000).sample_iter(rng);
-
-    (0..500).for_each(|_| {
-        grid.push(
-            (0..500)
-                .map(|_| generator.next().unwrap())
-                .collect::<Vec<_>>(),
-        );
-    });
-
-    // the input data is maxed to half the limits on each input according to leetcode constraints
+    println!("k: {}", k);
+    println!("number of submatrices: {}", count_submatrices_ref(&grid, k));
 
     let mut group = c.benchmark_group("all");
     group.measurement_time(std::time::Duration::from_secs(20));
