f95a077ef527f388a641332486715b295222ecb8
Olaf Schlüter <tallinn1960@yahoo.de>
2024-03-19 21:50:08 +0100

    better naming

--- cpp/count_submatrices.cpp
+++ cpp/count_submatrices.cpp
@@ -2,18 +2,18 @@
 
 int count_submatrices(std::vector<std::vector<int>>& matrix, int target) {
     int count = 0;
-    std::vector<int> row_sum(matrix[0].size(), 0);
+    std::vector<int> submatrix_sums(matrix[0].size(), 0);
     for (auto r : matrix) {
         int submatrices_in_row = 0;
-        row_sum[0] += r[0];
-        if (row_sum[0] > target) {
+        submatrix_sums[0] += r[0];
+        if (submatrix_sums[0] > target) {
             break;
         }
         submatrices_in_row++;
         for (int i = 1; i < r.size(); i++) {
             r[i] += r[i - 1];
-            row_sum[i]+= r[i];
-            if (row_sum[i] > target) {
+            submatrix_sums[i]+= r[i];
+            if (submatrix_sums[i] > target) {
                 break;
             }
             submatrices_in_row++;
